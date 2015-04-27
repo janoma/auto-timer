ceb720f2ea242c6a6fb8e891a3a193669f292d98
Alejandro Mallea <janoma@gmail.com>
2015-04-27 10:11:42 -0400

    Improves the test to show scope and constructors.

--- test.cpp
+++ test.cpp
@@ -1,14 +1,32 @@
 #include <cmath>
+#include <iostream>
 
 #include "auto_timer.h"
 
 int main()
 {
-    AutoTimer timer;
+    // Default constructor
+    AutoTimer timer0;
 
-    for (double i = 0; i < 100000000; ++i)
     {
-        double root = sqrt(i);
-        root *= root;
-    }
-}
+        // Using 9 decimals when printing the result
+        AutoTimer timer1(9);
+
+        for (double i = 0; i < 100000000; ++i)
+        {
+            double root = sqrt(i);
+            root *= root;
+        }
+    } // << timer1 will be destroyed here
+
+    {
+        // Explicit reference to std::cout, and 4 decimals
+        AutoTimer timer2(std::cout, 4);
+
+        for (int i = 0; i < 100000000; ++i)
+        {
+            int root = sqrt(i);
+            root *= root;
+        }
+    } // << This destroyes timer2
+} // Finally, timer0 is destroyed
